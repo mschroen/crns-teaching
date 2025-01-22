09a6de4154b5fa030fc6631f29819c419ae60c9c
Martin Schrön <post@mschroen.de>
2025-01-22 10:23:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ st.set_page_config(
 
 A simple Streamlit app to demonstrate exemplary CRNS data processing. Only for teaching purposes.
 
-*Authors: Daniel Power, Martin Schrön, Steffen Zacharias, Rafael Rosolem*
+*Authors: Daniel Power, Martin Schrön, Steffen Zacharias, Rafael Rosolem*  
 *Supported by Helmholtz Centre for Environmental Research, Leipzig, and University of Bristol, UK.*
 
 ## Data source
